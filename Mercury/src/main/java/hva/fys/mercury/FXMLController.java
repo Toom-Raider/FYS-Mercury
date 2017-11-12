6f110f07170813f290ab964771c305f80b7b0982
djmbritt <djmb102@gmail.com>
2017-11-12 20:23:19 +0100

    Working login and logout

--- Mercury/src/main/java/hva/fys/mercury/FXMLController.java
+++ Mercury/src/main/java/hva/fys/mercury/FXMLController.java
@@ -10,110 +10,52 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-//import javafx.scene.control.Label; 
 import javafx.scene.Parent;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-//import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-//import javafx.scene.layout.BorderPane; 
 
 public class FXMLController implements Initializable {
 
     @FXML
-    private StackPane workspace;
+    public TextField userTextField;
 
     @FXML
-    private BorderPane rootNode;
+    public PasswordField passTextField;
 
     @FXML
-    private StackPane rootWorkSpace;
+    public StackPane rootWorkSpace;
 
     @FXML
-    private TextField userTextField;
+    public BorderPane rootNode;
 
+    public StackPane getRootWorkSpace() {
+        return rootWorkSpace;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
     @FXML
-    private PasswordField passTextField;
-
-    @FXML
-    private void openRegistreerGevondenbagage(ActionEvent event) {
-        System.out.println("registreer bagage geopend ");
-        Parent pane = loadFXMLFile("/fxml/bagageFormulier.fxml");
-
-        System.out.println(pane);
-        System.out.println(workspace);
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
+    public void logOut(ActionEvent event){
+        System.out.println("logging out...");
+        Parent pane = loadFXMLFile("/fxml/Root.fxml");
+        rootNode.getChildren().clear();
+        rootNode.getChildren().setAll(pane);
     }
 
     @FXML
-    private void openRegistreerVerlorenbagage(ActionEvent event) {
-        System.out.println("verloren bagage geopend ");
-        Parent pane = loadFXMLFile("/fxml/registreerVermist.fxml");
-
-        System.out.println(pane);
-        System.out.println(workspace);
-
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void openDashboard(ActionEvent event) {
-        System.out.println("dashboard geopend ");
-        Parent pane;
-        pane = loadFXMLFile("/fxml/Dashboard.fxml");
-
-        System.out.println(pane);
-        System.out.println(workspace);
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
-
-    }
-
-    @FXML
-    private void openBagageZoeken(ActionEvent event) {
-        System.out.println("bagage zoeken geopend ");
-        Parent pane = loadFXMLFile("/fxml/bagageFormulier.fxml");
-
-        System.out.println(pane);
-        System.out.println(workspace);
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void openManagerOverzicht(ActionEvent event) {
-        System.out.println("manager  geopend ");
-        Parent pane = loadFXMLFile("/fxml/managerOverzicht.fxml");
-
-        System.out.println(pane);
-        System.out.println(workspace);
-        workspace.getChildren().clear();
-        workspace.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void logOut(ActionEvent event) {
-        System.out.println("Logging Out");
-        Parent pane = loadFXMLFile("/fxml/Login.fxml");
-
-        System.out.println("pane: " + pane);
-        System.out.println("rootWorkspace: " + rootWorkSpace);
-
-        rootWorkSpace.getChildren().clear();
-        rootWorkSpace.getChildren().setAll(pane);
-    }
-
-    @FXML
-    private void loginAction(ActionEvent event) {
+    public void loginAction(ActionEvent event) {
         System.out.println("Logging In");
         Parent pane = loadFXMLFile("/fxml/MenuBar.fxml");
 
-        System.out.println(pane);
-        System.out.println(rootWorkSpace);
+        System.out.println("pane:" + pane);
+        System.out.println("rootWorkspace: " + rootWorkSpace);
 
         String userNameString, passwordString, userPass = "admin";
 
@@ -121,16 +63,18 @@ public class FXMLController implements Initializable {
         passwordString = passTextField.getText();
 
         System.out.printf("\nuser: %s\tpass:%s.\n", userNameString, passwordString);
-        
+
         /*
         TODO
         Function for checking database for all users and check credentials
-        */
-
+         */
         //Checks credentials, which is admin!
         if (userNameString.equalsIgnoreCase(userPass) && passwordString.equalsIgnoreCase(userPass)) {
             rootWorkSpace.getChildren().clear();
-            rootWorkSpace.getChildren().add(pane);
+            rootWorkSpace.getChildren().setAll(pane);
+            System.out.println("rootworkspace: " + rootWorkSpace);
+            System.out.println("rootNode: " + rootNode);
+
         } else {
             String content = "Wrong credentials";
             System.out.println(content);
@@ -138,13 +82,20 @@ public class FXMLController implements Initializable {
             alert.setContentText(content);
             alert.showAndWait();
         }
-
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+//    dit is een idee
+//    @FXML
+//    public void exitLogOut(ActionEvent event) {
+//        System.out.println("Logging Out");
+//        Parent pane = loadFXMLFile("/fxml/Login.fxml");
+//
+//        System.out.println("pane: " + pane);
+//        System.out.println("rootWorkspace: " + rootWorkSpace);
+//
+//        rootWorkSpace.getChildren().clear();
+//        rootWorkSpace.getChildren().setAll(pane);
+//    }
 
     private Parent loadFXMLFile(String fxmlFileName) {
         try {
@@ -153,5 +104,9 @@ public class FXMLController implements Initializable {
             System.out.printf("\n%s: %s\n", ex.getClass().getName(), ex.getMessage());
             return null;
         }
+    }
+
+    public void printRoot() {
+        System.out.println("ThisrootNode: " + this.rootNode);
     }
 }
--- Mercury/src/main/java/hva/fys/mercury/MenuBarController.java
+++ Mercury/src/main/java/hva/fys/mercury/MenuBarController.java
@@ -5,10 +5,17 @@ package hva.fys.mercury;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * FXML Controller class
@@ -20,9 +27,100 @@ public class MenuBarController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private StackPane rootWorkSpace;
+
+    @FXML
+    private StackPane workspace;
+
+    @FXML
+    private BorderPane rootNode;
+
+    @FXML
+    private AnchorPane anchorWorkSpace;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        System.out.printf("\nStarting Menu Bar: %s\n", "initing...");
+        System.out.printf("\nresources: %s\n", resources);
+        System.out.printf("\nlocation: %s\n", location);
+    }
+
+
+    @FXML
+    private void openRegistreerGevondenbagage(ActionEvent event) {
+        System.out.println("registreer bagage geopend ");
+        Parent pane = loadFXMLFile("/fxml/bagageFormulier.fxml");
+
+        System.out.println("rootWorkspace: " + rootWorkSpace);
+
+        System.out.println(pane);
+        System.out.println(workspace);
+        workspace.getChildren().clear();
+        workspace.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void openRegistreerVerlorenbagage(ActionEvent event) {
+        System.out.println("verloren bagage geopend ");
+        Parent pane = loadFXMLFile("/fxml/registreerVermist.fxml");
+        System.out.println("rootWorkspace: " + rootWorkSpace);
+
+        System.out.println(pane);
+        System.out.println(workspace);
+
+        workspace.getChildren().clear();
+        workspace.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void openDashboard(ActionEvent event) {
+        System.out.println("dashboard geopend ");
+        Parent pane;
+        pane = loadFXMLFile("/fxml/Dashboard.fxml");
+        System.out.println("rootWorkspace: " + rootWorkSpace);
+
+        System.out.println(pane);
+        System.out.println(workspace);
+        workspace.getChildren().clear();
+        workspace.getChildren().setAll(pane);
+
+    }
+
+    @FXML
+    private void openBagageZoeken(ActionEvent event) {
+        System.out.println("bagage zoeken geopend ");
+        Parent pane = loadFXMLFile("/fxml/bagageFormulier.fxml");
+
+        System.out.println(pane);
+        System.out.println(workspace);
+        workspace.getChildren().clear();
+        workspace.getChildren().setAll(pane);
+    }
+
+    @FXML
+    private void openManagerOverzicht(ActionEvent event) {
+        System.out.println("manager  geopend ");
+        Parent pane = loadFXMLFile("/fxml/managerOverzicht.fxml");
+
+        System.out.println(pane);
+        System.out.println(workspace);
+        workspace.getChildren().clear();
+        workspace.getChildren().setAll(pane);
+    }
+
+    private Parent loadFXMLFile(String fxmlFileName) {
+        try {
+            return FXMLLoader.load(getClass().getResource(fxmlFileName));
+        } catch (IOException ex) {
+            System.out.printf("\n%s: %s\n", ex.getClass().getName(), ex.getMessage());
+            return null;
+        }
+    }
 }
